271cf8ededcb2cf4b47829e6933996775595c872
Sandesh <dongaonkarsandesh001@gmail.com>
2026-03-18 22:59:35 +0530

    Implements Armstrong Logic

---
+++ ArmstrongNumber.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int original = num;
+        int sum = 0;
+        int digits = 0;
+
+        // Step 1: Count digits
+        int temp = num;
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        // Step 2: Calculate sum of powers
+        temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+
+        // Step 3: Check Armstrong
+        if (sum == original) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not an Armstrong Number");
+        }
+
+        sc.close();
+    }
+}
